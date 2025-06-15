6b6f884fa82e4a992532eb23e24cf1455b5d44b9
unknown <masumajaffery1@gmail.com>
2025-06-15 20:21:53 +0500

    Add API

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector(".search-form-input");
-  let cityElement = document.querySelector(".weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
-let searchFormElement = document.querySelector(".search-form");
+function searchCity(city) {
+  let apiKey = "b4ed4ebd2tb54300df73af502odb4636";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
