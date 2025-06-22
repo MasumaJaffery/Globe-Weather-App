5483783c6bf85cd3212740639706ae688fa3a5cf
unknown <masumajaffery1@gmail.com>
2025-06-23 01:19:26 +0500

    Update UI

--- index.js
+++ index.js
@@ -100,3 +100,15 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Karbala");
+
+ const stars = document.getElementById('stars');
+  for (let i = 0; i < 100; i++) {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.width = `${Math.random() * 3}px`;
+    star.style.height = star.style.width;
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.animationDelay = `${Math.random() * 2}s`;
+    stars.appendChild(star);
+  }
